eaaba5313c7ac7d40e63681f3e9c1b18b9c21256
Fernanda Rocha <fernandaporocha@gmail.com>
2021-01-30 12:02:44 +0000

    Creating the Vector class

---
+++ src/ie/fernandarocha/Vector.java
@@ -0,0 +1,37 @@
+package ie.fernandarocha;
+
+import java.util.Arrays;
+
+public class Vector {
+	
+	private Student[] students = new Student[100];
+	
+	public void add(Student student) {
+		
+	}
+	
+	public void add(int position, Student student) {
+		
+	}
+	
+	public Student get(int position) {
+		
+	}
+	
+	public void remove(int position) {
+		
+	}
+	
+	public boolean contains(Student student) {
+		
+	}
+	
+	public int size() {
+		
+	}
+	
+	public String toString() {
+		return Arrays.toString(students);
+	}
+
+}
