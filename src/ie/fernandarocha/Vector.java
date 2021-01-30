3a21bd479ab8c0111fc0b2060ec9492897206297
Fernanda Rocha <fernandaporocha@gmail.com>
2021-01-30 12:07:54 +0000

    Implementing add method

--- src/ie/fernandarocha/Vector.java
+++ src/ie/fernandarocha/Vector.java
@@ -7,7 +7,12 @@ public class Vector {
 	private Student[] students = new Student[100];
 	
 	public void add(Student student) {
-		
+		for(int i = 0; i < this.students.length; i++) {
+			if(this.students[i]==null) {
+				this.students[i] = student;
+				break;
+			}
+		}	
 	}
 	
 	public void add(int position, Student student) {
