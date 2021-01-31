39bd98e60e62b65e4b6de78667931137d1362676
Fernanda Rocha <fernandaporocha@gmail.com>
2021-01-31 23:56:05 +0000

    Changing variable name

--- src/ie/fernandarocha/Vector.java
+++ src/ie/fernandarocha/Vector.java
@@ -3,12 +3,12 @@ package ie.fernandarocha;
 public class Vector {
 	
 	private Object[] objects = new Object[100];
-	private int studentTotal = 0;
+	private int totalOfObjects = 0;
 	
 	public void add(Student student) {
 		this.checkSize();
-		this.objects[this.studentTotal] = student;
-		this.studentTotal++;
+		this.objects[this.totalOfObjects] = student;
+		this.totalOfObjects++;
 	}
 	
 	public void add(int position, Student student) {
@@ -17,12 +17,12 @@ public class Vector {
 			throw new IllegalArgumentException("Invalid Position");
 		}
 		
-		for (int i = this.studentTotal -1; i >= position; i--) {
+		for (int i = this.totalOfObjects -1; i >= position; i--) {
 			this.objects[i+1] = this.objects[i];
 		}
 		
 		this.objects[position] = student;
-		this.studentTotal++;
+		this.totalOfObjects++;
 	}
 	
 	public Object get(int position) {
@@ -36,14 +36,14 @@ public class Vector {
 		if (!this.isPositonOccupied(position)) {
 			throw new IllegalArgumentException("Invalid position");
 		}
-		for(int i = position; i < this.studentTotal -1; i++) {
+		for(int i = position; i < this.totalOfObjects -1; i++) {
 			this.objects[i] = this.objects[i+1];
 		}
-		this.studentTotal--;
+		this.totalOfObjects--;
 	}
 	
 	public boolean contains(Student student) {
-		for (int i = 0; i < this.studentTotal; i++) {
+		for (int i = 0; i < this.totalOfObjects; i++) {
 			if (student.equals(this.objects[i])) {
 				return true;
 			}
@@ -52,42 +52,42 @@ public class Vector {
 	}
 	
 	public int size() {
-		return this.studentTotal;
+		return this.totalOfObjects;
 	}
 	
 	public String toString() {
 		
-		if(this.studentTotal == 0) {
+		if(this.totalOfObjects == 0) {
 			return "[]";
 		}
 		
 		StringBuilder builder = new StringBuilder();
 		builder.append("[");
 		
-		for (int i = 0; i < this.studentTotal -1; i++) {
+		for (int i = 0; i < this.totalOfObjects -1; i++) {
 			builder.append(this.objects[i]);
 			builder.append(", ");
 		}
 		
-		builder.append(this.objects[this.studentTotal -1]);
+		builder.append(this.objects[this.totalOfObjects -1]);
 		builder.append("]");
 		return builder.toString();
 	}
 	
 	private boolean isPositonOccupied(int position) {
-		return position >=0 && position < this.studentTotal;
+		return position >=0 && position < this.totalOfObjects;
 	}
 	
 	private boolean isValidPositon(int position) {
-		return position >=0 && position <= this.studentTotal;
+		return position >=0 && position <= this.totalOfObjects;
 	}
 	
 	private void checkSize() {
-		if(this.studentTotal == this.objects.length) {
+		if(this.totalOfObjects == this.objects.length) {
 			Object[] newArray = new Object[this.objects.length*2];
 			for (int i = 0; i < this.objects.length; i++) {
 				newArray[i] = this.objects[i];
-			}
+			} 
 			this.objects = newArray;
 		}
 	}
